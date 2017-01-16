37902bbea89d1452f1b8d7ba126e95740f4c5421
gomugom <power4ce@gmail.com>
2017-01-16 17:10:54 +0900

    eslint 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,7 +33,7 @@ module.exports = {
             "error",
             "always"
         ],
-        "no-unused-vars": 0,
-        "no-console": 0
+        "no-unused-vars": [0],
+        "no-console": [0]
     }
 };
