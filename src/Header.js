4cb09418869ebfc711e01b358a238eda13b79e5b
alakoko <alakoko.kim@gmail.com>
2017-02-11 12:55:05 +0900

    todoApp 1. add list, delete

---
+++ src/Header.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class Header extends Component{
+  handleKeyDown(e){
+    const text = e.target.value;
+
+    if(!text || e.keyCode !== 13) return;
+    this.props.addTodo(text);
+    e.target.value = '';
+  }
+  render(){
+    return(
+      <header>
+        <h1 className="todo-app__header">To Do List!!</h1>
+        <input
+            type="text"
+            className="todo-app__new-todo"
+            placeholder="뭐 할꺼양?"
+            onKeyDown={e => this.handleKeyDown(e)}
+        />
+      </header>
+    );
+  }
+}
+
+export default Header;
---
+++ src/TodoList.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import Todo from './Todo';
+
+class TodoList extends Component{
+
+  render(){
+    const todos = this.props.todos.map(todo =>(
+      <Todo
+        key = {`todo#${todo.id}`}
+        text = {todo.text}
+        isDone = {todo.isDone}
+        deleteTodo={()=> this.props.deleteTodo(todo.id)}
+      />
+    ));
+    return(
+      <div className="todo-app__main">
+        <div className="toggle-all" />
+          <ul className="todo-list">
+            {todos}
+          </ul>
+      </div>
+    );
+  }
+}
+
+export default TodoList;
