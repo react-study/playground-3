bc810887f0ec741978f71dda29f53aa44f1eb0a0
devjang <devjang2016@gmail.com>
2017-02-11 10:30:04 +0900

    test

---
+++ src/Parent.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Child from './Child';
+
+class Parent extends React.Component {
+	constructor() {
+		super();
+		this.state = {  list : [0]	}
+		this.addChild    = this.addChild.bind(this);
+		this.removeChild = this.removeChild.bind(this);
+	}
+	
+	addChild() {
+		const nextList = [...this.state.list];
+		nextList.push(nextList.length);
+		this.setState({ list : nextList });
+	}
+	
+	removeChild() {
+		const nextList = [...this.state.list];
+		nextList.pop();
+		this.setState({ list : nextList });
+	}
+	
+	render() {
+		if (!this.state.list.length) return (
+			<button onClick={this.addChild}>자식 추가</button>
+		);
+		return (
+			<div>
+				<Child list={this.state.list}/>
+				<button onClick={this.addChild}>자식 추가</button>
+				<button onClick={this.removeChild}>자식 삭제</button>
+			</div>
+		)
+	}
+}
+
+export default Parent;
