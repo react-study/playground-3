004bf6b986ee384bd22089c6ec3a2bd7138db29d
sajacaros <sajacall82@naver.com>
2017-02-11 11:51:06 +0900

    더미 데이터 추가 및 화면 구성

--- src/Todo.js
+++ src/Todo.js
@@ -7,7 +7,20 @@ import React, {
 
 class Todo extends Component {
     render() {
-        return('');
+        const {
+            text, isDone
+        } = this.props;
+        this.isDone;
+        return(
+            <li className="todo-item">
+                <div className="toggle" />
+                <div className="todo-item__view">
+                    <div className="todo-item__view__text">{text}</div>
+                    <button className="todo-item__destroy" />
+                </div>
+                <input type="text" className="todo-item__edit" />
+            </li>
+        );
     }
 }
 
--- src/TodoList.js
+++ src/TodoList.js
@@ -2,12 +2,39 @@
  * Created by sajacaros on 2017-02-11.
  */
 import React, {Component} from 'react';
+import Todo from './Todo';
+
+const data = [{
+    id:1,
+    text: 'hello world1',
+    isDone: true
+},{
+    id:2,
+    text: 'hello world2',
+    isDone: false
+},{
+    id:3,
+    text: 'hello world3',
+    isDone: true
+},{
+    id:4,
+    text: 'hello world4',
+    isDone: true
+}];
 
 class TodoList extends Component {
     render() {
+        const todos = data.map(todo =>(
+           <Todo
+               key={`todo#${todo.id}`}
+                text={todo.text}
+                isDone={todo.isDone}
+           />
+        ));
         return(
             <div className="todo-app__main">
                 <div className="toggle-all" />
+                   {todos}
                 <ul className="todo-list"></ul>
             </div>
         );
