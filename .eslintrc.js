64e0794497032bc789fcf100755d36bcfd85c8f4
최민성 <GreatMS@choeminseongui-MacBook-Pro.local>
2017-02-18 02:09:39 +0900

    Todo App 1

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true,
+        "commonjs": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+            "jsx": true,
+            "modules": true
+        },
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "no-unused-vars": [0],
+        "no-console": [0]
+    }
+};
--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-//----- main.js -----
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Parent from './Parent'
+import App from './App';
+
 ReactDOM.render(
-    (<Parent />),
+    <App />,
     document.getElementById('root')
 );
