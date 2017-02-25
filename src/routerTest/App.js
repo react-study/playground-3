9f4d2fb8d2ef7982ab032ba8c40633d0d187f3a6
minsora <ssola129@gmail.com>
2017-02-25 11:22:57 +0900

    라우터 예제

---
+++ src/routerTest/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Router, Route, IndexRoute, Redirect, browserHistory } from 'react-router';
+
+import Container from './Container';
+import { Home, About, Name, Portfolio } from './Component';
+
+class App extends React.Component {
+    render() {
+        return (
+            <Router history={browserHistory}>
+                <Route path="/" component={Container}>
+                    <IndexRoute component={Home} />
+                    <Route path="about" component={About}>
+                        <Route path="name" component={Name} />
+                        <Route path="redirect0"
+                            onEnter={(nextState, replace) => replace('/portfolio/1')}
+                        />
+                        <Redirect from ="redirect1" to="/portfolio/2"></Redirect>
+                    </Route>
+                    <Route path="portfolio(/:id)" component={Portfolio}/>
+                </Route>
+            </Router>
+        );
+    }
+};
+
+export default App;
