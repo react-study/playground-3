a4b02ae7074b740a7ca6b861549bf59a54827b51
gomugom <power4ce@gmail.com>
2017-02-25 11:21:00 +0900

    라우터 예제

---
+++ src/routerTest/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Router, Route, IndexRoute, Redirect, browserHistory } from 'react-router';
+
+import Container from './Container';
+import { Home, About, Name, Portfolio } from './Component';
+
+class App extends React.Component {
+    render() {
+        return (
+            <Router history={browserHistory}>
+                <Route path="/" component={Container}>
+                    <IndexRoute component={Home} />
+                    <Route path="about" component={About}>
+                        <Route path="name" component={Name} />
+                        <Route path="redirect0"
+                            onEnter={(nextState, replace) => replace('/portfolio/1')}
+                        />
+                        <Redirect from ="redirect1" to="/portfolio/2"></Redirect>
+                    </Route>
+                    <Route path="portfolio(/:id)" component={Portfolio}/>
+                </Route>
+            </Router>
+        );
+    }
+};
+
+/*
+
+/                 : Home <IndexRoute>
+/about            : About <Route>
+/about/name       : Name  <Route> <Route>
+/about/redirect0  : redirect to /portfolio/1 <Route>
+/about/redirect1  : redirect to /portfolio/2 <Redirect>
+/portfolio        : Portfolio <Route>
+/portfolio/id     : Portfolio 한테 id값을 'routeParams'으로 넘김. (생략가능)
+*/
+
+export default App;
---
+++ src/routerTest/Container.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const Container = ({ children }) => (
+    <div>
+        <header>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/about/name">About - Name</Link></li>
+                <li><Link to="/about/redirect0">About - RedirectTo: Portfolio #1</Link></li>
+                <li><Link to="/about/redirect1">About - RedirectTo: Portfoli #2</Link></li>
+                <li><Link to="/portfolio">Portfolio - All</Link></li>
+                <li><Link to="/portfolio/1">Portfolio - #1</Link></li>
+                <li><Link to="/portfolio/2">Portfolio - #2</Link></li>
+                <li><Link to="/portfolio/3">Portfolio - #3</Link></li>
+            </ul>
+        </header>
+
+        {children}
+    </div>
+);
+
+export default Container;
