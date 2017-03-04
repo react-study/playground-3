507c4c078885d938daab0261a7c09d6d4f101a45
devjang <devjang2016@gmail.com>
2017-03-04 10:17:16 +0900

    MongoDB!

--- src/Footer.js
+++ src/Footer.js
@@ -1,50 +1,50 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 import ClassNames from 'classnames';
 
 class Footer extends Component {
-    render() {
-        const {
-            filterName,
-            activeLength,
-            completeLength,
-            deleteCompleted,
-            selectFilter
-        } = this.props;
-        const filters = ['All', 'Active', 'Completed'];
-
-        return (
-            <div className="footer">
+	render() {
+		const {
+			      filterName,
+			      activeLength,
+			      completeLength,
+			      deleteCompleted,
+			      selectFilter
+		      }       = this.props;
+		const filters = ['All', 'active', 'completed'];
+		
+		return (
+			<div className="footer">
                 <span className="todo-count">
                     <strong>{activeLength}</strong>
-                    {' '}
-                    item{activeLength === 1 ? ' ' : 's '}
-                    left
+	                {' '}
+	                item{activeLength === 1 ? ' ' : 's '}
+	                left
                 </span>
-                <ul className="todo-filters">
-                    {filters.map(v => (
-                        <li key={`filter#${v}`}>
-                            <a
-                                href="#"
-                                className={ClassNames({
-                                    'selected': filterName === v
-                                })}
-                                onClick={() => selectFilter(v)}
-                            >{v}</a>
-                        </li>
-                    ))}
-                </ul>
-                <button
-                    className={[
-                        "todo-delete-completed",
-                        completeLength ? '' : "hidden"
-                    ].join(' ')}
-                    onClick={deleteCompleted}
-                >
-                    Clear Completed
-                </button>
-            </div>
-        );
-    }
+				<ul className="todo-filters">
+					{filters.map(v => (
+						<li key={`filter#${v}`}>
+							<Link
+								to={`/${v}`}
+								className={ClassNames({
+									'selected' : (filterName === v) || (!v && !filterName)
+								})}
+							>{v ? v.replace(/^\w/, v=> v.toUpperCase()) : 'All'}</Link>
+						</li>
+					))}
+				</ul>
+				<button
+					className={[
+						"todo-delete-completed",
+						completeLength ? '' : "hidden"
+					].join(' ')}
+					onClick={deleteCompleted}
+				>
+					Clear Completed
+				</button>
+			</div>
+		);
+	}
 }
 
 export default Footer;
