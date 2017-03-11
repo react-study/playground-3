7728fc9d46a46face8b573124804f50583e6ba83
minsora <ssola129@gmail.com>
2017-03-11 10:52:17 +0900

    리덕스 예제 00

---
+++ src/reduxExample/InputBox.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class InputBox extends React.Component {
+    handleClick(type){
+        this.props[type](this._input.value);
+        this._input.value = '';
+        this._input.focus();
+    }
+    render() {
+        return (
+            <div>
+                <input type="number" ref={ref => {this._input = ref}} />
+                <button onClick={() => this.handleClick('save')}>입금</button>
+                <button onClick={() => this.handleClick('withdraw')}>출금</button>
+            </div>
+        )
+    };
+}
+
+export default InputBox;
